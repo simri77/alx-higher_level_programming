f6af184ac2cab8718bc8dfde5f66b36d6a83ec91
simri77 <simretmesfin97@gmail.com>
2022-06-02 11:00:15 -0700

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    size = len(sys.argv)
+    sum = 0
+    if size > 1:
+        for i in range(1, size):
+            sum += int(sys.argv[i])
+    print("{}".format(sum))
